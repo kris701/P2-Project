daa871df140261bf1ead6c039b48a0d57a9d98ae
Martin-Thomsen <msth19@student.aau.dk>
2020-03-25 12:31:15 +0100

    Added Node.js server structure
    Renamed P2ProjektServer to P2ProjektTestserver.
    Created a new folder called P2ProjektServer and added a new visual studio project, containing the file structure for the Node.js server.

---
+++ P2ProjektServer/P2ProjektServer/README.md
@@ -0,0 +1,3 @@
+# P2ProjektServer
+
+
---
+++ P2ProjektServer/P2ProjektServer/interface.js
@@ -0,0 +1,3 @@
+let prediction = require("./PredictionAlgorithms.js");
+let sensorInfo = require("./SimpleSensorInfo.js");
+let warningAndSolution = require("./WarningAndSolutionSelectionAlgorithm");
