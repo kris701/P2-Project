2a428a8a5b9eafc9c83c494d41225ee7b8f5862c
Rasmus Secher <R.Secher@live.dk>
2020-03-30 12:54:29 +0200

    Enabled the server to feed the selector
    The servers information regarding the number of rooms and names of these are now shown in the selector of the website.

--- P2Projekt/P2Projekt/app.js
+++ P2Projekt/P2Projekt/app.js
@@ -1,8 +1,38 @@
-﻿async function jsonFetch(url) {
+﻿try {
+    let fetchedData;
 
-    let response = await fetch(url);
-    let myJson = await response.json();
-    console.log(myJson);
-}
+    async function jsonFetch(url) {
+        let response = await fetch(url);
+        fetchedData = await response.json();
 
-jsonFetch("http://localhost:5000/getsensorinfo").catch(e => console.log(e));
+        console.log(fetchedData);
+        await importDataToSelect();
+    }
+
+    jsonFetch("http://localhost:5000/getsensorinfo").catch(e => console.log(e));
+
+    //Adds more elements to the select in the html for room selection
+    async function importDataToSelect() {
+        let roomSelect = document.getElementById("selectedRoom");
+
+        for (let i = 0; i < fetchedData.length; i++) {
+            let option = document.createElement("option");
+            option.text = fetchedData[i].RoomName;
+            roomSelect.add(option);
+        }
+    }
+
+    //Activates once a new room has been selected
+    function roomChangeFunction() {
+        let roomSelect = document.getElementById("selectedRoom");
+        let roomData = fetchedData[roomSelect.selectedIndex];
+
+        displayRoomData(roomData);
+    }
+
+    function displayRoomData(roomData) {
+        let dataDisplay = document.getElementById("dataDisplaySingle");
+        dataDisplay.style.color = red;
+    }
+
+} catch (err) {console.log(err)}
