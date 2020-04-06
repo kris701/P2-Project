1f3cf2b7ab3931f6c1715ff9fd837412bf541a32
Daniel Møller <57629796+Danielm0402@users.noreply.github.com>
2020-04-06 22:40:47 +0200

    Update adming login
    updated login.hrml with ids for username, password and the sumbit button.
    updated login.js to get fetch the inputs and store dem in cookies

--- P2Projekt/P2Projekt/pages/adminloginpage/login.js
+++ P2Projekt/P2Projekt/pages/adminloginpage/login.js
@@ -1,1 +1,60 @@
 ﻿
+
+function login () {
+    let username = document.getElementById("input_username").value;
+    let password = document.getElementById("input_password").value;
+    setLogin(username, password);
+    let nextPage = "/"
+    window.location.href = appendProperties(nextPage, getlogin())
+}
+
+document.getElementById("login_submit").onclick = login
+
+
+// Retrived from https://stackoverflow.com/questions/14573223/set-cookie-and-get-cookie-with-javascript
+function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+// Retrived from https://stackoverflow.com/questions/14573223/set-cookie-and-get-cookie-with-javascript
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+// Retrived from https://stackoverflow.com/questions/14573223/set-cookie-and-get-cookie-with-javascript
+function eraseCookie(name) {
+    document.cookie = name + '=; Max-Age=-99999999;';
+}
+
+function setLogin(username, password) {
+    setCookie("username", username, 7);
+    setCookie("password", password, 7);
+}
+
+function getlogin() {
+    return {
+        username: getCookie("username"),
+        password: getCookie("password")
+    }
+}
+
+function appendProperties(url, properties) {
+    for (let key in properties) {
+        if (!url.contains("?")) url += "?";
+        else url += "&";
+
+        url += `${encodeURIComponent(key)}=${encodeURIComponent(properties[key])}`;
+    }
+    return url;
+}
