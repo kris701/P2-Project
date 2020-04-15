c7a0608d9969a38a8580f0d1fdc9ed59f370d899
Kristian Skov <kris701kj@gmail.com>
2020-04-15 12:05:18 +0200

    Update WarningAndSolutionSelectionAlgorithm.js
    * Fixed WAS section

--- P2Project/ServerSide/WarningAndSolutionSelectionAlgorithm.js
+++ P2Project/ServerSide/WarningAndSolutionSelectionAlgorithm.js
@@ -13,16 +13,12 @@ class SolutionInfo {
         this.Message = Message;
     }
 }
-class WarningItem {
-    constructor(Message, SolutionInfo) {
+class WarningInfo {
+    constructor(WarningID, SensorType, Message, SolutionInfo) {
+        this.WarningID = WarningID;
+        this.SensorType = SensorType;
         this.Message = Message;
         this.SolutionInfo = SolutionInfo;
-    }
-}
-class WarningInfo {
-    constructor(SensorType, WarningItems) {
-        this.SensorType = SensorType;
-        this.WarningItems = WarningItems;
     }
 }
 class ReturnClass {
@@ -30,6 +26,13 @@ class ReturnClass {
         this.Data = Data;
     }
 }
+class PriorityInfo {
+    constructor(Priority, TimeUntil) {
+        this.Priority = Priority;
+        this.TimeUntil = TimeUntil;
+    }
+}
+const NoWarnMessasge = new WarningInfo(-2, -2, "No Warnings", new SolutionInfo(0, "No Solution"));
 
 const PriorityEnum = {
     None: 0,
@@ -58,6 +61,8 @@ module.exports.WASC = class {
         await BCC.asyncForEach(predictionDataArray.Data, async function (v) {
             returnItem = await getWASForEachThesholdPass(v, predictionDataArray.Interval, returnItem);
         });
+        if (returnItem.Data.length == 0)
+            returnItem.Data.push(NoWarnMessasge);
 
         return returnItem;
     }
@@ -69,55 +74,77 @@ async function getWASForEachThesholdPass(predictionData, interval, returnItem) {
     await BCC.asyncForEach(predictionData.ThresholdPasses, async function (v) {
         let priority = getPriority(v.TimeUntil, interval);
 
-        if (priority != PriorityEnum.None) {
-            let warningInfo = await getWarningInfoQuery(predictionData.SensorType, priority);
-            returnItem.Data.push(warningInfo);
+        if (priority.Priority != PriorityEnum.None) {
+            let isThere = await checkIfWarningIsThere(predictionData.SensorType, returnItem.Data, priority.Priority);
+
+            if (!isThere) {
+                let warningInfo = await getWarningInfoQuery(predictionData.SensorType, priority);
+                if (warningInfo.Message != "" && warningInfo.SolutionInfo.Message != "")
+                    returnItem.Data.push(warningInfo);
+            }
         }
     });
 
     return returnItem;
 }
 
+async function checkIfWarningIsThere(sensorType, searchArray, priority) {
+    let isThere = false;
+
+    await BCC.asyncForEach(searchArray, async function (v) {
+        if (v.SensorType == sensorType) {
+            if (v.SolutionInfo.WarningPriority < priority) {
+                v.SolutionInfo = await getSolutionQuery(v.WarningID, priority);
+            }
+            isThere = true;
+        }
+    });
+
+    return isThere;
+}
+
 function getPriority(timeUntilBadIAQ, interval) {
+
     let minutes = timeUntilBadIAQ * interval;
 
     if (minutes > 60)
-        return PriorityEnum.None;
+        return new PriorityInfo(PriorityEnum.None, minutes);
     else if (minutes > 30)
-        return PriorityEnum.Low;
+        return new PriorityInfo(PriorityEnum.Low, minutes);
     else if (minutes > 10)
-        return PriorityEnum.Medium;
+        return new PriorityInfo(PriorityEnum.Medium, minutes);
     else
-        return PriorityEnum.High;
+        return new PriorityInfo(PriorityEnum.High, minutes);
 
-    return PriorityEnum.None;
+    return new PriorityInfo(PriorityEnum.None, minutes);
 }
 
 async function getWarningInfoQuery(sensorType, priority) {
-    let result = new WarningInfo(sensorType, []);
+    let result = new WarningInfo(-1, sensorType, "", new SolutionInfo(priority, ""));
 
     let queryTable = await BCC.MakeQuery("SELECT * FROM [Warnings] WHERE [SensorType]=@sensorTypeInput", [new BCC.QueryValue("sensorTypeInput", sql.Int, sensorType)]);
-    await BCC.asyncForEach(queryTable.recordset, async function (v) {
-        let solutionInfo = await getSolutionQuery(v.WarningID, priority);
-        let warningItem = new WarningItem(v.Message, solutionInfo);
-        result.WarningItems.push(warningItem);
-    });
+
+    if (queryTable.recordset.length != 0) {
+        result.Message = queryTable.recordset[0].Message;
+        result.WarningID = queryTable.recordset[0].WarningID;
+        result.SolutionInfo = await getSolutionQuery(queryTable.recordset[0].WarningID, priority);
+    }
 
     return result;
 }
 
 async function getSolutionQuery(warningID, priority) {
-    let result = [];
+    let result = new SolutionInfo(priority, "");
 
     let queryTable = await BCC.MakeQuery(
         "SELECT * FROM [Solutions] WHERE [WarningID]=@warningIDInput AND [WarningPriority]=@priorityInput",
         [new BCC.QueryValue("warningIDInput", sql.Int, warningID),
-         new BCC.QueryValue("priorityInput", sql.Int, priority)]
+        new BCC.QueryValue("priorityInput", sql.Int, priority.Priority)]
     );
-    await BCC.asyncForEach(queryTable.recordset, async function (v) {
-        let solutionItem = new SolutionInfo(priority, v.Message);
-        result.push(solutionItem);
-    });
+
+    if (queryTable.recordset.length != 0) {
+        result.Message = queryTable.recordset[0].Message;
+    }
 
     return result;
 }
